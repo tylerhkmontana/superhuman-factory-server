91f3303342bd3e97850cd29ac3b5e793b6de4ab5
tylerhkmontana <tylerhkmontana@gmail.com>
2024-05-09 00:47:23 -0400

    implemented new user added

--- server.js
+++ server.js
@@ -3,25 +3,50 @@ const app = express();
 const port = process.env.PORT || 8080;
 require("dotenv").config();
 
+const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.post("/api/newUser", async (req, res) => {
-  console.log(process.env.DATABASE_URL);
-  console.log("Testing");
+app.get("/api/getUsers", async (req, res) => {
   try {
-    const createTable = await pool.query(
-      "CREATE TABLE IF NOT EXISTS users(userId INT PRIMARY KEY, email VARCHAR NOT NULL, family_name VARCHAR, given_name VARCHAR, joined DATE NOT NULL)"
-    );
-    console.log(createTable);
-    res.send("Table created");
+    const data = await pool.query("SELECT * FROM users");
+    res.json(data.rows);
   } catch (error) {
-    // console.log(error);
-    res.status(500).send("Failed to create table");
+    console.log(error);
+    res.status(500).send("Failed to retrieve users");
+  }
+});
+
+app.post("/api/newUser", async (req, res) => {
+  const { email, givenName, familyName, pr } = req.body;
+
+  try {
+    const data = await pool.query(
+      `SELECT * FROM users WHERE email = '${email}';`
+    );
+    if (data.rows.length > 0) {
+      res.status(400).send("The user already exists");
+    } else {
+      const joined = new Date().toISOString();
+      console.log(pr);
+      await pool.query(
+        `INSERT INTO users (email, family_name, given_name, joined, pr) 
+        VALUES ('${email}', '${familyName}', '${givenName}', '${joined}', '${JSON.stringify(
+          pr
+        )}')`
+      );
+      res.send("New user was successfully added.");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Failed to add new User");
   }
 });
 
