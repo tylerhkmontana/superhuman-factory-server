18f6a0f050ff4dc2beb111e943558e3f0ba73b42
tylerhkmontana <tylerhkmontana@gmail.com>
2024-05-09 19:02:25 -0400

    update user pr implemented

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const cors = require("cors");
 require("dotenv").config();
 
 const bodyParser = require("body-parser");
 const { Pool } = require("pg");
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -24,29 +31,45 @@ app.get("/api/getUsers", async (req, res) => {
   }
 });
 
-app.post("/api/newUser", async (req, res) => {
-  const { email, givenName, familyName, pr } = req.body;
+// Register New User
+app.post("/api/user/newUser", async (req, res) => {
+  const { email, given_name, family_name } = req.body;
 
   try {
     const data = await pool.query(
       `SELECT * FROM users WHERE email = '${email}';`
-    );
+    ); // Check if the user already exists in the database
     if (data.rows.length > 0) {
-      res.status(400).send("The user already exists");
+      console.log("user exists");
+      res.send("The user already exists");
     } else {
       const joined = new Date().toISOString();
-      console.log(pr);
       await pool.query(
-        `INSERT INTO users (email, family_name, given_name, joined, pr) 
-        VALUES ('${email}', '${familyName}', '${givenName}', '${joined}', '${JSON.stringify(
-          pr
-        )}')`
+        `INSERT INTO users (email, family_name, given_name, joined) 
+        VALUES ('${email}', '${family_name}', '${given_name}', '${joined}')`
       );
       res.send("New user was successfully added.");
     }
   } catch (error) {
     console.log(error);
     res.status(500).send("Failed to add new User");
+  }
+});
+
+app.put("/api/user/updatePr", async (req, res) => {
+  const { newPr, user } = req.body;
+
+  try {
+    await pool.query(`
+        UPDATE users SET pr = '${JSON.stringify(newPr)}' WHERE email = '${
+      user.email
+    }'
+    `);
+
+    res.send("User PR successfully updated");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Failed to update the PR");
   }
 });
 
